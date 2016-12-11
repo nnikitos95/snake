b676d682b35b37aaa7c677cdb18f659be9639b12
nnikitos95 <nnikitos95@mail.ru>
2016-12-11 20:31:13 +0700

    рефакторинг кода, создание нового класса Walls для реаризации постоение границ, и реализация методов проверки пересечения змейки с границами и с самой собой

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,18 +14,8 @@ namespace Snake
             int H = Console.WindowHeight;
             int W = Console.WindowWidth;
             Console.SetBufferSize(W,H);
-           /* Point p1 = new Point(1, 3, '*');
-            Point p2 = new Point(2, 5, '#');
-            p1.Draw();
-            p2.Draw();*/
-            HorizontalLine hltop = new HorizontalLine(0,W-1,H-1,'+');
-            VerticalLine vlleft = new VerticalLine(0, H-1, 0, '+');
-            HorizontalLine hlbottom = new HorizontalLine(0, W - 1, 0, '+');
-            VerticalLine vlright = new VerticalLine(0, H - 1, W-1, '+');
-            hltop.Draw();
-            vlleft.Draw();
-            hlbottom.Draw();
-            vlright.Draw();
+            Walls walls = new Walls(W, H);
+            walls.Draw();
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
@@ -34,6 +24,7 @@ namespace Snake
             food.Draw();
             while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail()) break;
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Walls
+    {
+        List<Figure> walls;
+        public Walls(int W,int H)
+        {
+            walls = new List<Figure>();
+            HorizontalLine hltop = new HorizontalLine(0, W - 1, H - 1, '+');
+            VerticalLine vlleft = new VerticalLine(0, H - 1, 0, '+');
+            HorizontalLine hlbottom = new HorizontalLine(0, W - 1, 0, '+');
+            VerticalLine vlright = new VerticalLine(0, H - 1, W - 1, '+');
+            walls.Add(hltop);
+            walls.Add(vlleft);
+            walls.Add(hlbottom);
+            walls.Add(vlright);
+        }
+        public void Draw()
+        {
+            foreach (var wall in walls) wall.Draw();
+        }
+
+        public bool IsHit(Figure figure)
+        {
+            foreach(var wall in walls)
+            {
+                if (wall.IsHit(figure)) return true;
+            }
+            return false;
+        }
+    }
+}
