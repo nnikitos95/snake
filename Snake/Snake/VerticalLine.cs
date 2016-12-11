e01c0b534c2f3ad17877252dece6d35881fd622e
nnikitos95 <nnikitos95@mail.ru>
2016-12-11 16:00:34 +0700

    Добален класс VerticalLine

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+        public VerticalLine(int yT, int yB, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yT; y < yB; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+        public void Draw()
+        {
+            foreach (Point p in pList)
+                p.Draw();
+        }
+    }
+}
